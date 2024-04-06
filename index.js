f44f9ce5d63fed5aaa6eaa01044a9b69c34d276f
Robin <rrlangton@gmail.com>
2024-04-06 16:01:58 -0500

    index.js questions

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What's your project's title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Write a description of your project (what?, why?, how?)",
+      name: "description",
+    },
+    {
+        type: "input",
+        message: "How can I use this application? (include video links and screen shots)",
+        name: "usage",
+      },
+    {
+        type: "input",
+        message: "How do I install or run the project/application?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What sources did I use?",
+        name: "sources",
+      },
+  ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function fs.writeToFile('index.js', data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
