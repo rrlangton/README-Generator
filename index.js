31a1e587dc32b2252454b04e03d3db36de232590
Robin <rrlangton@gmail.com>
2024-04-06 17:08:55 -0500

    application works

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What's your project's title?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Write a description of your project (what?, why?, how?)",
-      name: "description",
-    },
-    {
-        type: "input",
-        message: "How can I use this application? (include video links and screen shots)",
-        name: "usage",
-      },
-    {
-        type: "input",
-        message: "How do I install or run the project/application?",
-        name: "installation",
-      },
-      {
-        type: "input",
-        message: "What sources did I use?",
-        name: "sources",
-      },
-  ])
 
-// TODO: Create a function to write README file
-function fs.writeToFile('index.js', data) {
+// Function to generate README content
+function generateReadme(answers) {
+    // Implement the logic to generate README content based on user answers
+    // Example:
+    return `
+# ${answers.title}
 
+## Description
+${answers.description}
+
+## Usage
+${answers.usage}
+
+## Installation
+${answers.installation}
+
+## Sources
+${answers.sources}
+`;
 }
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What's your project's title?",
+                name: "title",
+            },
+            {
+                type: "input",
+                message: "Write a description of your project (what?, why?, how?)",
+                name: "description",
+            },
+            {
+                type: "input",
+                message: "How can I use this application? (include video links and screen shots)",
+                name: "usage",
+            },
+            {
+                type: "input",
+                message: "How do I install or run the project/application?",
+                name: "installation",
+            },
+            {
+                type: "input",
+                message: "What sources did I use?",
+                name: "sources",
+            },
+        ])
+        .then((answers) => {
+            const readmeContent = generateReadme(answers);
+
+            fs.promises.writeFile('README.md', readmeContent)
+                .then(() => console.log('Successfully created README.md!'))
+                .catch((err) => console.error(err));
+        })
+        .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
+
